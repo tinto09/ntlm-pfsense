b2932cf47c3c8321708258c4e411af17f4e6a88a
tinto09 <ignacio@neon.com.uy>
2020-07-29 19:11:28 -0300

    Update ntlm2pfsense.sh

--- ntlm2pfsense.sh
+++ ntlm2pfsense.sh
@@ -35,7 +35,7 @@ mkdir -p /var/db/samba4/winbindd_privileged
 chown -R :proxy /var/db/samba4/winbindd_privileged
 chmod -R 0750 /var/db/samba4/winbindd_privileged
 
-fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/pf2ad/pf2ad/2.4.3-SAMBA4/samba.inc
+fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/tinto09/ntlm-pfsense/master/samba.inc
 fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/pf2ad/pf2ad/2.4.3-SAMBA4/samba.xml
 
 /usr/local/sbin/pfSsh.php <<EOF
